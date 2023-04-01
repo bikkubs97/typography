ac1f910962f536de47d5161b101d454e23b77ee6
Bikku BS <bikku4444@gmail.com>
2023-04-01 11:01:02 +0530

    cors

--- server/server.js
+++ server/server.js
@@ -14,14 +14,7 @@ const openai = new OpenAIApi(configuration)
 
 const app = express()
 
-app.use((req, res, next) => {
-   
-    res.header ('Access-Control-Allow-Origin', '*')
-  res.header ('Access-Control-Allow-Credentials', true)
-  res.header ('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS')
-  res.header ('Access-Control-Allow-Headers', 'Content-Type')
-    next();
-  });
+app.use(cors())
   
 app.use(express.json())
 
