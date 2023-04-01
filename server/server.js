7a283b731de0784add67a97c9d19d29d0cee88c7
Bikku BS <bikku4444@gmail.com>
2023-04-01 11:23:37 +0530

    msg

--- server/server.js
+++ server/server.js
@@ -17,7 +17,14 @@ const app = express()
 app.use(express.json())
 
 
-app.use(cors())
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Methods', 'POST');
+    next();
+  });
+
+  
 
 
 
